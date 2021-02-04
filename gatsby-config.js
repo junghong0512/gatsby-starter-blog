e86ed80afbea42c2dd2d4a647e2de4de412a89b8
junghong <junghong@ninewatt.com>
2021-02-04 11:13:42 +0900

    gatsby-config updated

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,7 +51,6 @@ module.exports = {
           `gatsby-remark-prismjs`,
           `gatsby-remark-copy-linked-files`,
           `gatsby-remark-smartypants`,
-          `gatsby-plugin-netlify-cms`,
         ],
       },
     },
